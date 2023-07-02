9916382e863bb2868c050eb98b4af5720e2581e7
Bless Ziamah <tziamah1@gmail.com>
2023-07-02 18:51:45 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <>
         <nav className="navbar navbar-expand-sm navbar-dark bg-transparent py-2 fixed-top">
             <div className="container-fluid ">
-                <span className="navbar-brand font-weight-bolder">HBVK Executive Beach Resort</span>
+                <span className="navbar-brand font-weight-bolder">Flor Purpura Executive Beach Resort</span>
                 <a href="void(0)" className="navbar-toggler border-0" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span>
                         <FaAlignRight className="nav-icon" /></span>
